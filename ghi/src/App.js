35c095d46fef5944c12ab09cc39445201f60602f
Dahna Ahn <dahnaahn16@gmail.com>
2023-05-23 20:59:15 +0000

    Added frontend for trainer view

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,51 +1,38 @@
-import { useEffect, useState } from "react";
-import Construct from "./Construct.js";
-import ErrorNotification from "./ErrorNotification";
+
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
-import TrainerHome from './TrainerHome';
-import CustomerHome from './CustomerHome';
+import TrainerHome from './TrainerView/TrainerHome';
+import CustomerHome from './CustomerView/CustomerHome';
+import RoomsList from "./TrainerView/RoomsList";
+import Mainpage from "./MainPageView/Mainpage";
+import PetsList from "./TrainerView/PetsList";
+import TrainingClass from "./TrainerView/TrainingClass";
+import TrainerList from "./TrainerView/TrainerList";
+import TrainingBoardingHistory from "./TrainerView/TrainingBoardingHistory";
 
 function App() {
   return (
     <>
     <div className="flex">
-      {/* <TrainerHome /> */}
-      <CustomerHome />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="trainer/" element={<TrainerHome />}>
+            <Route path="rooms/" element={<RoomsList />} />
+            <Route path="pets/" element={<PetsList />} />
+            <Route path="training/" element={<TrainingClass />} />
+            <Route path="trainers/" element={<TrainerList />} />
+            <Route path="history/" element={<TrainingBoardingHistory />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
     </div>
     </>
   );
 }
-// function App() {
-//   const [launchInfo, setLaunchInfo] = useState([]);
-//   const [error, setError] = useState(null);
 
-//   useEffect(() => {
-//     async function getData() {
-//       let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-//       console.log("fastapi url: ", url);
-//       let response = await fetch(url);
-//       console.log("------- hello? -------");
-//       let data = await response.json();
-
-//       if (response.ok) {
-//         console.log("got launch data!");
-//         setLaunchInfo(data.launch_details);
-//       } else {
-//         console.log("drat! something happened");
-//         setError(data.message);
-//       }
-//     }
-//     getData();
-//   }, []);
-
-//   return (
-//     <div>
-//       <ErrorNotification error={error} />
-//       <Construct info={launchInfo} />
-//     </div>
-//   );
-// }
 
 export default App;
---
+++ ghi/src/TrainerView/TrainingBoardingHistory.js
@@ -0,0 +1,9 @@
+function TrainingBoardingHistory(){
+    return (
+      <div style={{ paddingLeft: "20rem", marginTop: "-50rem" }}>
+        <p> this is the training and boarding history </p>
+      </div>
+    );
+}
+
+export default TrainingBoardingHistory
