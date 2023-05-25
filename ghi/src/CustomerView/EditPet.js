ba9f4cb8169e788ae2f1ace932cb62ee881616b7
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-05-25 11:25:28 -0700

    Created edit pet page in links - to continue working on the form page

--- ghi/src/CustomerView/EditPet.js
+++ ghi/src/CustomerView/EditPet.js
@@ -4,29 +4,31 @@ import { useParams } from "react-router-dom";
 // import useUser from "./useUser";
 
 function EditPet() {
-  const [user, setUser] = useState([]);
-  const [pet, setPets] = useState([]);
+  const [pet, setPet] = useState([]);
+  const [formData, setFormData] = useState({
+    Name: "",
+    Breed: "",
+    Gender: "",
+    Age: "",
+    Picture: "",
+    Size: "",
+    Weight: "",
+    Diet: "",
+  });
 
   // put this in the NAV
   // const { token, logout } = useToken();
   // const { user } = useUser(token);
 
-  const { user_id } = useParams();
+  const { user_id, pet_id } = useParams();
   const fetchData = async () => {
-    const userURL = `http://localhost:8000/api/accounts/${user_id}/`;
-    const petsURL = "http://localhost:8000/api/pets";
+    const petURL = `http://localhost:8000/api/pets/${pet_id}/`;
 
-    const [userResponse, petsResponse] = await Promise.all([
-      fetch(userURL),
-      fetch(petsURL),
-    ]);
+    const petResponse = await fetch(petURL);
 
-    if (userResponse.ok && petsResponse.ok) {
-      const userData = await userResponse.json();
-      const petsData = await petsResponse.json();
-      console.log(userData, petsData);
-      setUser(userData);
-      setPets(petsData);
+    if (petResponse.ok) {
+      const petData = await petResponse.json();
+      setPet(petData);
     } else {
       console.log("Error fetching data");
     }
@@ -36,10 +38,139 @@ function EditPet() {
     fetchData();
   }, []);
 
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Determine the fields that have changed
+    const updatedFields = {};
+    for (const key in formData) {
+      if (formData[key] !== pet[key]) {
+        updatedFields[key] = formData[key];
+      }
+    }
+
+    // If no fields have changed
+    if (Object.keys(updatedFields).length === 0) {
+      alert("No changes have been made.");
+      return;
+    }
+  };
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
-    <div>
-      <p> We are editing a pet</p>
-    </div>
+    <p> This is the edit page </p>
+    // <div className="form-container">
+    //   <div className="secondary-container">
+    //     <form onSubmit={handleSubmit} id="create-user-form">
+    //       <div className="mb-5">
+    //         <label htmlFor="name" className="label-css">
+    //           Class Name
+    //         </label>
+    //         <input
+    //           required
+    //           onChange={nameChange}
+    //           type="text"
+    //           name="name"
+    //           id="name"
+    //           placeholder="Class Name"
+    //           className="form-input-container"
+    //           value={name}
+    //         />
+    //       </div>
+    //       <div className="form-wrap">
+    //         <div className="label-container">
+    //           <div className="mb-5">
+    //             <label htmlFor="category" className="label-css">
+    //               Category
+    //             </label>
+    //             <input
+    //               required
+    //               onChange={categoryChange}
+    //               type="text"
+    //               name="category"
+    //               id="category"
+    //               placeholder="...  ...  ..."
+    //               className="form-input-container"
+    //               value={category}
+    //             />
+    //           </div>
+    //         </div>
+    //         <div className="label-container">
+    //           <div className="mb-5">
+    //             <label htmlFor="max_attendees" className="label-css">
+    //               Max Attendees
+    //             </label>
+    //             <input
+    //               required
+    //               onChange={maxAttendeeChange}
+    //               type="number"
+    //               name="max_attendees"
+    //               id="max_attendees"
+    //               placeholder="Max Attendees"
+    //               className="form-input-container"
+    //               value={maxAttendees}
+    //             />
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <div className="mb-5">
+    //         <label htmlFor="start_datetime" className="label-css">
+    //           Start Date/Time
+    //         </label>
+    //         <input
+    //           required
+    //           onChange={startChange}
+    //           type="datetime-local"
+    //           name="start_datetime"
+    //           id="start_datetime"
+    //           placeholder="datetiem"
+    //           className="form-input-container"
+    //           value={start}
+    //         />
+    //       </div>
+    //       <div className="mb-5">
+    //         <label htmlFor="end_datetime" className="label-css">
+    //           End Date/Time
+    //         </label>
+    //         <input
+    //           required
+    //           onChange={endChange}
+    //           type="datetime-local"
+    //           name="end_datetime"
+    //           id="end_datetime"
+    //           placeholder="datetime"
+    //           className="form-input-container"
+    //           value={end}
+    //         />
+    //       </div>
+    //       <div className="mb-5">
+    //         <label htmlFor="description" className="label-css">
+    //           Description
+    //         </label>
+    //         <input
+    //           required
+    //           onChange={descChange}
+    //           type="text"
+    //           name="description"
+    //           id="description"
+    //           placeholder="Treats are Great"
+    //           className="form-input-container"
+    //           value={description}
+    //         />
+    //       </div>
+    //       <div>
+    //         <button className="submit-button">Submit</button>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
   );
 }
 
