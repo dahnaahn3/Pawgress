feb6cc3bff313e2103c15e3f789f2b368e7f2790
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-05-31 18:47:18 -0700

    Updated width of nav bar container to have buttons/icons extend end-to-end

--- ghi/src/MainPageView/MainPageNav.js
+++ ghi/src/MainPageView/MainPageNav.js
@@ -12,7 +12,7 @@ function MainPageNav() {
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex mx-auto p-4">
+      <div className="max-w-screen-3xl flex mx-auto p-4">
         <a href="/" className="flex">
           <img
             src={
