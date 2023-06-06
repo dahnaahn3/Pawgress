e95064a2273ad5c7bb708818c2ae72684b506883
lrningcode12 <117692502+lrningcode12@users.noreply.github.com>
2023-06-06 10:16:45 -0700

    Cleaned up class references on signup form

--- ghi/src/MainPageView/SignupForm.js
+++ ghi/src/MainPageView/SignupForm.js
@@ -78,8 +78,8 @@ function SignupForm () {
     return (
       <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div class="absolute inset-0 bg-[#93CBE1] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="absolute inset-0 bg-[#93CBE1] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <form onSubmit={handleSubmit} id="create-user-form">
               <div className="form-wrap">
                 <div className="label-container">
